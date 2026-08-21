0c2f4b34a51b26203b3586f18094bcdb9053aedf
Regina Williams <regina.williams@web.de>
2026-08-21 13:49:23 +0200

    Test

--- core/src/main/java/de/erik/pokemonfangame/Main.java
+++ core/src/main/java/de/erik/pokemonfangame/Main.java
@@ -4,8 +4,15 @@ import com.badlogic.gdx.Game;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Main extends Game {
+
     @Override
     public void create() {
+
+
+        System.out.print ("Hello World2");
         setScreen(new FirstScreen());
     }
+
+
+
 }
